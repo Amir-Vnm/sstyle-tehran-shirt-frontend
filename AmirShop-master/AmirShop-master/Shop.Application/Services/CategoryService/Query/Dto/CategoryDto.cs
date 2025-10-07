9a73235190ff823ca7c83887543f16a102fdfb19
vanishcode <vanishcodex@email.com>
2025-10-07 18:53:51 +0330

    hello

--- AmirShop-master/AmirShop-master/Shop.Application/Services/CategoryService/Query/Dto/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shop.Application.Services.CategoryService.Query.Dto
-{
-    public record CategoryDto
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string ImageUrl { get; set; }
-    }
-}
